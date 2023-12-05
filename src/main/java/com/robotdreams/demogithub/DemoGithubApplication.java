ed89de0645e58dd6077406aaff38a0fb5586277d
sadikalgul <ytuloyal@gmail.com>
2023-12-05 22:47:10 +0300

    feat: sadikal - initial commit

---
+++ src/main/java/com/robotdreams/demogithub/DemoGithubApplication.java
@@ -0,0 +1,13 @@
+package com.robotdreams.demogithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robotdreams/demogithub/DemoGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robotdreams.demogithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
